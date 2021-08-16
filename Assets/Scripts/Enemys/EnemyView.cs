f57868116ec8e5387298b2182d06bce846eac4cb
Igor Poplavsky <resip@tut.by>
2021-08-16 18:55:05 +0300

    Add basic enemy animation

--- Assets/Scripts/Enemys/EnemyView.cs
+++ Assets/Scripts/Enemys/EnemyView.cs
@@ -10,6 +10,7 @@ namespace PlayerNamaspase.Enemys
     {
         [HideInInspector] public CollisionDetected _player;
         public ConfigurationEnemy enemyConfig;
+        public EnemyState _state;
         private NavMeshAgent _agent;
         private Animator _animator;
         private bool isMove = false;
@@ -28,6 +29,24 @@ namespace PlayerNamaspase.Enemys
             if (isMove)
             {
                 _agent.SetDestination(_player.transform.position);
+            }
+
+            UpdateAnimator();
+        }
+
+        private void UpdateAnimator()
+        {
+            switch (_state)
+            {
+                case EnemyState.Walk:
+                    _animator.Play("Armature|Walk_Cycle_2");
+                    break;
+                case EnemyState.Run:
+                    _animator.Play("Armature|Walk_Cycle_2");
+                    break;
+                default:
+                    _animator.Play("Armature|Rest_1");
+                    break;
             }
         }
     }
